cb88f43b93a44e4ae7121b4f13ccb177b08c66bf
Anjali Uttarkar <anjaliuttarkar@gmail.com>
2026-08-05 10:28:05 +0530

    Time: 11 ms (90.75%), Space: 12.3 MB (53.68%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -6,5 +6,7 @@ class Solution(object):
         """
         if n==0 or n==1:
             return n
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+        a,b=0,1
+        for i in range(2,n+1):
+            a,b=b,a+b
+        return b
