c3050632ee3b37dc915d70d0ccbc6baf94e1f96a
Anjali Uttarkar <anjaliuttarkar@gmail.com>
2026-08-05 10:00:31 +0530

    Time: 4 ms (53.74%), Space: 12.2 MB (92.24%) - LeetHub

---
+++ 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sumOfGoodIntegers(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        ans=0
+        for x in range(max(1,n-k),n+k+1):
+            if(n&x)==0:
+                ans+=x
+        return ans        
